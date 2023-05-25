9fc6ec31b351f9557cc6a738fbdb58d664384c97
system <system.opsi@example.com>
2023-05-25 18:08:29 +0700

    commit editTime2

--- src/main/java/com/demo/page/autoOrangeHRM/timePage.java
+++ src/main/java/com/demo/page/autoOrangeHRM/timePage.java
@@ -22,24 +22,75 @@ public class timePage extends navigationPage{
     @FindBy(xpath = "//button[text()=' Edit ']")
     private WebElement btnEdit;
 
-    @FindBy(xpath = "(//input[@placeholder='Type for hints...'])[1]")
+    @FindBy(xpath = "(//input[@placeholder='Type for hints...'])")
     private WebElement selectProject;
 
     @FindBy(xpath = "//span[text()='Apache Software Foundation - ASF - Phase 1']")
     private WebElement clickAutoComplete;
 
-    @FindBy(xpath = "//div[@class='oxd-select-text oxd-select-text--active oxd-select-text--error']")
+    @FindBy(xpath = "//div[@class='oxd-select-text-input']")
     private WebElement selectActivity;
 
-    @FindBy(xpath = "//span[text()='Feature Development']")
+    //div[contains(@class,'oxd-select-text oxd-select-text')]
+
+    
+
+    @FindBy(xpath = "//span[text()='QA Testing']")
     private WebElement clickAutoActivity;
 
-    @FindBy(xpath = "//span[text()='Feature Development']")
-    private WebElement clickAutoActivity1;
+    //begin - input timesheets
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[2]")
+    private WebElement inputTime1;
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[3]")
+    private WebElement inputTime2;
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[4]")
+    private WebElement inputTime3;
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[5]")
+    private WebElement inputTime4;
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[6]")
+    private WebElement inputTime5;
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[7]")
+    private WebElement inputTime6;
+
+    @FindBy(xpath = "(//input[contains(@class,'oxd-input oxd-input')])[8]")
+    private WebElement inputTime7;
+
+    //Verify time
+    @FindBy(xpath = "//span[text()='02:00']")
+    private WebElement getTimeSheet1;
+
+    @FindBy(xpath = "//span[text()='05:00']")
+    private WebElement getTimeSheet2;
+
+    @FindBy(xpath = "//span[text()='07:00']")
+    private WebElement getTimeSheet3;
+
+    @FindBy(xpath = "//span[text()='01:00']")
+    private WebElement getTimeSheet4;
+
+    @FindBy(xpath = "//span[text()='04:00']")
+    private WebElement getTimeSheet5;
+
+    @FindBy(xpath = "//span[text()='00:00']")
+    private WebElement getTimeSheet6;
+
+    @FindBy(xpath = "//span[text()='08:00']")
+    private WebElement getTimeSheet7;
+
+
+    //end - input timesheets
 
     @FindBy(xpath = "//button[@type='submit']")
     private WebElement btnSave;
 
+    @FindBy(xpath = "//p[text()='Successfully Saved']")
+    private WebElement verifySuccess;
 
     WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(),10);
 
@@ -80,12 +131,96 @@ public class timePage extends navigationPage{
     }
 
     public void clickAutoActivity() {
+        wait.until(ExpectedConditions.elementToBeClickable(clickAutoActivity));
         clickAutoActivity.click();
     }
 
+    //begin - enter timesheets
+
+    public void enterTime1(Integer enter1) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime1));
+        inputTime1.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        //inputTime1.click();
+        inputTime1.sendKeys(Integer.toString(enter1));
+    }
+
+    public void enterTime2(Integer enter2) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime2));
+        inputTime2.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        // inputTime2.clear();
+        inputTime2.sendKeys(Integer.toString(enter2));
+    }
+
+    public void enterTime3(Integer enter3) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime3));
+        inputTime3.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        // inputTime3.clear();
+        inputTime3.sendKeys(Integer.toString(enter3));
+    }
+
+    public void enterTime4(Integer enter4) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime4));
+        inputTime4.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        // inputTime4.clear();
+        inputTime4.sendKeys(Integer.toString(enter4));
+    }
+
+    public void enterTime5(Integer enter5) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime5));
+        inputTime5.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        // inputTime5.clear();
+        inputTime5.sendKeys(Integer.toString(enter5));
+    }
+
+    public void enterTime6(Integer enter6) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime6));
+        inputTime6.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        // inputTime6.clear();
+        inputTime6.sendKeys(Integer.toString(enter6));
+    }
+
+    public void enterTime7(Integer enter7) {
+        wait.until(ExpectedConditions.elementToBeClickable(inputTime7));
+        inputTime7.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.DELETE));
+        // inputTime7.clear();
+        inputTime7.sendKeys(Integer.toString(enter7));
+    }
+
+    public void enterAllColumnTime(Integer enter1, Integer enter2, Integer enter3, Integer enter4, Integer enter5, 
+    Integer enter6, Integer enter7) {
+        this.enterTime1(enter1);
+        this.enterTime2(enter2);
+        this.enterTime3(enter3);
+        this.enterTime4(enter4);
+        this.enterTime5(enter5);
+        this.enterTime6(enter6);
+        this.enterTime7(enter7);
+    }
+
+    //end - enter timesheets
+
     public void clickbtnSave() {
+        wait.until(ExpectedConditions.elementToBeClickable(btnSave));
         btnSave.click();
     }
+
+    
+    //verify success
+    public boolean isSuccessDisplayed() {
+        Boolean isSuccessDisplayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(verifySuccess));
+            return isSuccessDisplayed = true;
+        } catch (Exception e) {
+            return isSuccessDisplayed;
+        }
+    }
+
+    public String getSuccess() {
+        return verifySuccess.getText();
+    }
+
+    //verify select project
 
     public boolean isSelectProjectDisplayed() {
         Boolean isSelectProjectDisplayed = false;
@@ -101,6 +236,7 @@ public class timePage extends navigationPage{
         return clickAutoComplete.getText();
     }
 
+    //verify select activity
     public boolean isSelectActivityDisplayed() {
         Boolean isSelectActivityDisplayed = false;
         try {
@@ -114,4 +250,111 @@ public class timePage extends navigationPage{
     public String getActivityName() {
         return clickAutoActivity.getText();
     }
+
+    //time 1
+    public boolean isTime1Displayed() {
+        Boolean isTime1Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet1));
+            return isTime1Displayed = true;
+        } catch (Exception e) {
+            return isTime1Displayed;
+        }
+    }
+
+    public String getTime1() {
+        return getTimeSheet1.getText();
+    }
+
+    //time2
+    public boolean isTime2Displayed() {
+        Boolean isTime2Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet2));
+            return isTime2Displayed = true;
+        } catch (Exception e) {
+            return isTime2Displayed;
+        }
+    }
+
+    public String getTime2() {
+        return getTimeSheet2.getText();
+    }
+
+    //time3
+    public boolean isTime3Displayed() {
+        Boolean isTime3Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet3));
+            return isTime3Displayed = true;
+        } catch (Exception e) {
+            return isTime3Displayed;
+        }
+    }
+
+    public String getTime3() {
+        return getTimeSheet3.getText();
+    }
+
+    //time4
+    public boolean isTime4Displayed() {
+        Boolean isTime4Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet4));
+            return isTime4Displayed = true;
+        } catch (Exception e) {
+            return isTime4Displayed;
+        }
+    }
+
+
+    public String getTime4() {
+        return getTimeSheet4.getText();
+    }
+
+        //time5
+    public boolean isTime5Displayed() {
+        Boolean isTime5Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet5));
+            return isTime5Displayed = true;
+        } catch (Exception e) {
+            return isTime5Displayed;
+        }
+    }
+
+    public String getTime5() {
+        return getTimeSheet5.getText();
+    }
+
+    //time6
+    public boolean isTime6Displayed() {
+        Boolean isTime6Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet6));
+            return isTime6Displayed = true;
+        } catch (Exception e) {
+            return isTime6Displayed;
+        }
+    }
+
+    public String getTime6() {
+        return getTimeSheet6.getText();
+    }
+
+    //time7
+    public boolean isTime7Displayed() {
+        Boolean isTime7Displayed = false;
+        try {
+            wait.until(ExpectedConditions.visibilityOf(getTimeSheet2));
+            return isTime7Displayed = true;
+        } catch (Exception e) {
+            return isTime7Displayed;
+        }
+    }
+
+    public String getTime7() {
+        return getTimeSheet7.getText();
+    }
+
 }
--- src/test/java/com/demo/autoOrangeHRM/EditTimeSheet.java
+++ src/test/java/com/demo/autoOrangeHRM/EditTimeSheet.java
@@ -47,8 +47,22 @@ public class EditTimeSheet extends BaseTest{
         // click auto complete activity
         timePage.clickAutoActivity();
 
+        //begin - input timesheet
+
+        timePage.enterAllColumnTime(2, 5, 7, 1, 4, 0, 8);
+        // Thread.sleep(10000);
+
+        //end - input timesheet
+
         // click button save
         timePage.clickbtnSave();
+        // Thread.sleep(10000);
+
+        //Verify success
+        Assert.assertTrue(timePage.isSuccessDisplayed());
+
+        String getSuccess = timePage.getSuccess();
+        System.out.println("Verify: " + getSuccess); 
 
         //verify Apache Software Foundation - ASF - Phase 1
         Assert.assertTrue(timePage.isSelectProjectDisplayed());
@@ -62,6 +76,50 @@ public class EditTimeSheet extends BaseTest{
 
         String getNameAct = timePage.getActivityName();
         System.out.println("Activity name: " + getNameAct);
-     
+
+        //Verify Time 1
+        Assert.assertTrue(timePage.isTime1Displayed());
+
+        String getTime1 = timePage.getTime1();
+        System.out.println("Time 1: " + getTime1);
+
+        //Verify Time 2
+        Assert.assertTrue(timePage.isTime2Displayed());
+
+        String getTime2 = timePage.getTime2();
+        System.out.println("Time 2: " + getTime2);
+
+        //Verify Time 3
+        Assert.assertTrue(timePage.isTime3Displayed());
+
+        String getTime3 = timePage.getTime3();
+        System.out.println("Time 1: " + getTime3);
+
+        //Verify Time 4
+        Assert.assertTrue(timePage.isTime4Displayed());
+
+        String getTime4 = timePage.getTime4();
+        System.out.println("Time 4: " + getTime4);
+
+        //Verify Time 5
+        Assert.assertTrue(timePage.isTime5Displayed());
+
+        String getTime5 = timePage.getTime5();
+        System.out.println("Time 5: " + getTime5);
+
+        //Verify Time 6
+        Assert.assertTrue(timePage.isTime6Displayed());
+
+        String getTime6 = timePage.getTime6();
+        System.out.println("Time 6: " + getTime6);
+
+        //Verify Time 7
+        Assert.assertTrue(timePage.isTime7Displayed());
+
+        String getTime7 = timePage.getTime7();
+        System.out.println("Time 7: " + getTime7);
+
+
+   
     }
 }
